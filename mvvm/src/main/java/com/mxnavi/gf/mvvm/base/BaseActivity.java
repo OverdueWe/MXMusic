e9c7af9ca141069a73468061d8f96313fef61725
Mark <guofeng@meixing.com>
2018-12-03 11:09:47 +0800

    autosize
    add resolution adaptation module

--- mvvm/src/main/java/com/mxnavi/gf/mvvm/base/BaseActivity.java
+++ mvvm/src/main/java/com/mxnavi/gf/mvvm/base/BaseActivity.java
@@ -7,6 +7,9 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
 
+import com.mxnavi.gf.autosize.internal.CustomAdapt;
+import com.mxnavi.gf.mvvm.util.MxLog;
+
 /**
  * describle : Base of Activity
  *
@@ -14,7 +17,9 @@ import android.view.WindowManager;
  * @date 2018.11.30
  */
 
-public abstract class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity implements CustomAdapt{
+
+    private static final String TAG = "BaseActivity";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,12 +30,51 @@ public abstract class BaseActivity extends AppCompatActivity {
         initToolBar();
     }
 
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        MxLog.d(TAG,"onRestart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        MxLog.d(TAG,"onStop");
+    }
+
     /**
      * 空实现此方法 避免Activity异常销毁后 造成画页重叠
      * @param savedInstanceState
      */
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        MxLog.d(TAG,"onRestoreInstanceState");
+    }
+
+    /**
+     * 是否按照宽度进行等比例适配 (为了保证在高宽比不同的屏幕上也能正常适配, 所以只能在宽度和高度之中选一个作为基准进行适配)
+     *
+     * @return {@code true} 为按照宽度适配, {@code false} 为按照高度适配
+     */
+    @Override
+    public boolean isBaseOnWidth() {
+        return false;
+    }
+
+    /**
+     * 这里使用 IPhone 的设计图, IPhone 的设计图尺寸为 750px * 1334px, 高换算成 dp 为 667 (1334px / 2 = 667dp)
+     * <p>
+     * 返回设计图上的设计尺寸, 单位 dp
+     * {@link #getSizeInDp} 须配合 {@link #isBaseOnWidth()} 使用, 规则如下:
+     * 如果 {@link #isBaseOnWidth()} 返回 {@code true}, {@link #getSizeInDp} 则应该返回设计图的总宽度
+     * 如果 {@link #isBaseOnWidth()} 返回 {@code false}, {@link #getSizeInDp} 则应该返回设计图的总高度
+     * 如果您不需要自定义设计图上的设计尺寸, 想继续使用在 AndroidManifest 中填写的设计图尺寸, {@link #getSizeInDp} 则返回 {@code 0}
+     *
+     * @return 设计图上的设计尺寸, 单位 dp
+     */
+    @Override
+    public float getSizeInDp() {
+        return 0;
     }
 
     /**
---
+++ mvvm/src/main/java/com/mxnavi/gf/mvvm/util/MxLog.java
@@ -0,0 +1,47 @@
+package com.mxnavi.gf.mvvm.util;
+
+import android.util.Log;
+
+/**
+ * describle : Log封装
+ *
+ * @author Mark
+ * @date 2018.12.03
+ */
+
+public class MxLog {
+
+    private static final String TAG = "MxLog";
+
+    private static boolean debug;
+
+    private MxLog() {
+        throw new IllegalStateException("you can't instantiate me!");
+    }
+
+    public static boolean isDebug() {
+        return debug;
+    }
+
+    public static void setDebug(boolean debug) {
+        MxLog.debug = debug;
+    }
+
+    public static void d(String tag, String message) {
+        if (debug) {
+            Log.d(tag, message);
+        }
+    }
+
+    public static void w(String tag, String message) {
+        if (debug) {
+            Log.w(tag, message);
+        }
+    }
+
+    public static void e(String tag, String message) {
+        if (debug) {
+            Log.e(tag, message);
+        }
+    }
+}
